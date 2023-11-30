c5678ad685d3dbb7c598bde8711765a74255c5dd
Björn Hjortsten <bjorn.hjortsten@billogram.com>
2023-11-30 10:35:21 +0100

    Moved translations to their own files.
    Translations are now loaded from their own dedicated files. This is done via the http-backend plugin for i18-next.
    
    Added a npm-serve config for ease of use and a favicon for completeness.

--- public/app.js
+++ public/app.js
@@ -1,25 +1,18 @@
 import i18next from '../node_modules/i18next/dist/esm/i18next.js';
+import Backend from '../node_modules/i18next-http-backend/esm/index.js';
 
-await i18next.init({
-    lng: 'en',
-    debug: true,
-    resources: {
-        en: {
-            translation: {
-                "greeting": "Hello world!"
-            }
-        },
-        de: {
-            translation: {
-                "greeting": "Hallo Welt!"
-            }
-        },
-        sv: {
-            translation: {
-                "greeting": "Hej världen!"
-            }
+await i18next
+    .use(Backend)
+    .init({
+        lng: 'en',
+        supportedLngs: ['en', 'de', 'sv'],
+        fallbackLng: 'en',
+        debug: true,
+        ns: ['translation'],
+        defaultNS: 'translation',
+        backend: {
+            loadPath: 'locales/{{lng}}/{{ns}}.json'
         }
-    }
-});
+    });
 
 document.getElementById('greeting').innerHTML = i18next.t('greeting');
